fef2c17420fac11fe2197af02c45e212ed29b624
joelqwh <33408852+joelqwh@users.noreply.github.com>
2018-02-19 16:10:50 +0800

    amended the S

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
-import { DisplayComponent } from './component/display/display.component';
+import { DisplayComponent } from './components/display/display.component';
 
 
 @NgModule({
