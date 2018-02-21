18797e1d2930c8a1ef276809998d158956907142
joelqwh <33408852+joelqwh@users.noreply.github.com>
2018-02-21 18:01:18 +0800

    added search component but trying to find out why res is unparseable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -16,7 +17,9 @@ import { DisplayComponent } from './components/display/display.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Injectable, OnInit, Output, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Search} from './search';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
+@Injectable()
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('searchGIFForm')
+  searchGIFForm: NgForm;
+
+  @Output()
+  searching: EventEmitter<Search> = new EventEmitter<Search>();
+
+  private apiURL = 'http://api.giphy.com/v1/gifs/search?api_key=Kf4yZpbunyjW6DWn3632kA71xib2z74c&q=';
+  gifData = [];
+  http: HttpClient;
+
+  constructor( http: HttpClient ) { this.http = http; }
 
   ngOnInit() {
-  }
+
+    }
+
+    search (searchTerm: HTMLInputElement): void {
+    const link = this.apiURL + this.searchGIFForm.value;
+
+    this.http.get(link).subscribe((res: Response) => {
+      console.log(res);
+      this.gifData = res as any[];
+      console.log(this.gifData);
+
+    });
+    }
+  // search(): Promise {
+  // console.log(this.searchGIFForm.value);
+  //
+  // const promise = new Promise ((resolve, reject) => {
+  //   const url = this.apiURL + this.searchGIFForm.value;
+  //   this.http.get(url).toPromise().then(res => {
+  //     // console.log(res.json());
+  //     resolve();
+  //   });
+  // });
+  // return promise;
+
+  // }
+
+
+
 
 }
