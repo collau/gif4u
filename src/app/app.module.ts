588fba7bb5f8fea77bd5c9c58af44daec8db1a06
collau <collau@hotmail.com>
2018-02-22 13:07:50 +0800

    added display component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { DisplayComponent } from './components/display/display.component';
+import { GifsComponent } from './components/gifs.component';
+import { Gif4uService } from '../../gif4u.service';
+import { UserComponent } from './components/user/user.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    DisplayComponent
+    DisplayComponent,
+    GifsComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
@@ -23,7 +28,7 @@ import { DisplayComponent } from './components/display/display.component';
     ReactiveFormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ Gif4uService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
